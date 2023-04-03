fc1375b1ad6c95deeb98dc479d3f991638e0a2e7
JosueValbuena <joshvlbn@gmail.com>
2023-04-03 11:42:45 -0400

    estilos terminados, error al agregar a favoritos

--- src/components/Galeria.jsx
+++ src/components/Galeria.jsx
@@ -1,13 +1,26 @@
-import React, { useContext } from 'react'
+import React, { useContext, useState } from 'react'
 import { Mycontext } from '../context/MyContext'
+import HeartSvg from './HeartSvg';
 
 const Galeria = () => {
     const fotos = useContext(Mycontext);
-    console.log(fotos.data)
+    const [liked, setLiked] = useState(false)
+
+    const handleClick = (e) => {
+      let likedIndex = fotos.data.findIndex((ele) => ele.id === e);
+      let prueba = !fotos.data[likedIndex].favorito;
+      console.log(likedIndex, fotos.data)
+      console.log(prueba)
+    }
+
   return (
     <div className='container'>
       {fotos.data.map( (ele)=> 
-      <div className='card' key={ele.id} style={{backgroundImage: `url(${ele.src})`}}>
+      <div className='card' 
+      key={ele.id} 
+      style={{backgroundImage: `url(${ele.src})`}}
+      onClick={() => handleClick(ele.id)}>
+        <HeartSvg like={ele.favorito}/>
         <p>Autor: {ele.autor}</p>
       </div>)}
     </div>
